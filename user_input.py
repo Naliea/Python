1e36a1968b5b8d821c6f2f2b27febacf5078e6f9
Naliea <125865293+Naliea@users.noreply.github.com>
2024-02-26 11:36:55 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = int(input("What is your age?"))
+location = str(input("What is your location?"))
+print("Hello", name,"your age is", age , "and you live in", location)
